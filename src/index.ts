181b3498fc4d7c8459eb3f0cac558176505103e6
Nicholas-py <nicholas.waslander@gmail.com>
2026-07-15 21:27:59 -0400

    Deploy! Discover! And play games! Discover the world, with a touch of the pen.

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { DurableObject } from "cloudflare:workers";
  */
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject<Env> {
+export class UniversalStore extends DurableObject<Env> {
 	/**
 	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
 	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
@@ -34,7 +34,21 @@ export class MyDurableObject extends DurableObject<Env> {
 	 * @returns The greeting to be sent back to the Worker
 	 */
 	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
+		if (name != "stored") {
+			return `Hello, ${name}!`;
+		}
+		else {
+			return `Uh. TBD. BRB.`
+		}
+	}
+
+	async getValue(key:string): Promise<unknown> {
+		let val = await this.ctx.storage.get(key);
+		return val;
+	}
+
+	async setValue(key:string, value:string|number|boolean) {
+		await this.ctx.storage.put(key, value);
 	}
 }
 
@@ -53,7 +67,9 @@ export default {
 		//
 		// Requests from all Workers to the Durable Object instance named "foo"
 		// will go to a single remote Durable Object instance.
-		const stub = env.MY_DURABLE_OBJECT.getByName("foo");
+		const stub = env.UNIVERSAL_STORE.getByName("foo");
+
+		
 
 		// Call the `sayHello()` RPC method on the stub to invoke the method on
 		// the remote Durable Object instance.
